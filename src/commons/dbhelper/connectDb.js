9bdb2c49f9e1c90d718f764cd7d3543db90c3d5e
navinmishra1717 <navinmishra1717@gmail.com>
2021-02-03 10:31:18 +0545

    db setup and crud for user added

---
+++ src/commons/dbhelper/connectDb.js
@@ -0,0 +1,14 @@
+function connectDb(mongoose, config) {
+  return mongoose.connect(config.db.MONGO_SERVER_CONN_STR, {
+    autoReconnect: true,
+    reconnectTries: config.db.reconnectTries,
+    reconnectInterval: config.db.reconnectInterval,
+    poolSize: config.db.poolSize,
+    keepAlive: 1,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    // useUnifiedTopology: true,
+  });
+}
+
+module.exports = connectDb;
